154a49fa3f8d98b8ab57745965de712cd45831a4
Ivan <e4drcf@gmail.com>
2022-05-22 08:38:10 +0300

    refactored

--- main.go
+++ main.go
@@ -1,73 +1,122 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strings"
 	"time"
 )
 
+//Question struct that stores question with answer
+type Question struct {
+	question string
+	answer   string
+}
+
 func main() {
+
+	fileName, timeLimit := readArguments()
+	f, err := openFile(fileName)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	questions, err := readCSV(f)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if questions == nil {
+		return
+	}
+	score, err := askQuestion(questions, timeLimit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Your Score %d/%d\n\n", score, len(questions))
+
+}
+func readArguments() (fileName string, timeLimit int) {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format 'question, answer'")
-	timeLimit := flag.Int("linit", 30, "the time limit for the quiz in seconds")
+	limit := flag.Int("limit", 5, "the time limit for the quiz in seconds")
 	flag.Parse()
+	return *csvFileName, *limit
+}
 
-	file, err := os.Open(*csvFileName)
+func readCSV(f io.Reader) ([]Question, error) {
+	questions, err := csv.NewReader(f).ReadAll()
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file %s, %s", *csvFileName, err))
+		return nil, err
 	}
-	_ = file
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse the provided csv file")
+	amount := len(questions)
+	if amount == 0 {
+		return nil, fmt.Errorf("no question in file")
 	}
-	problems := parseLines(lines)
-	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
-	correct := 0
+	var data []Question
+	for _, line := range questions {
+		ques := Question{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+		data = append(data, ques)
+	}
+	return data, err
+}
 
-problemLoop:
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		answerCh := make(chan string)
-		go func() {
-			var answer string
-			fmt.Scanf("%s", &answer)
-			answerCh <- answer
-		}()
+func openFile(fileName string) (io.Reader, error) {
+	return os.Open(fileName)
+}
 
+func getInput(input chan string) {
+	for {
+		in := bufio.NewReader(os.Stdin)
+		result, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input <- result
+	}
+}
+
+func askQuestion(questions []Question, timeLimit int) (int, error) {
+	totalScore := 0
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	done := make(chan string)
+
+	go getInput(done)
+
+	for i := range questions {
+		ans, err := eachQuestion(questions[i].question, questions[i].answer, timer.C, done)
+		if err != nil && ans == -1 {
+			return totalScore, nil
+		}
+		totalScore += ans
+
+	}
+	return totalScore, nil
+}
+
+func eachQuestion(Quest string, answer string, timer <-chan time.Time, done <-chan string) (int, error) {
+	fmt.Printf("%s: ", Quest)
+	for {
 		select {
-		case <-timer.C:
-			fmt.Println()
-			break problemLoop
-		case answer := <-answerCh:
-			if answer == p.a {
-				correct++
+		case <-timer:
+			return -1, fmt.Errorf("time out")
+		case ans := <-done:
+			score := 0
+			if strings.Compare(strings.Trim(strings.ToLower(ans), "\n"), answer) == 0 {
+				score = 1
+			} else {
+				return 0, fmt.Errorf("wrong Answer")
 			}
-		}
-		fmt.Printf("You scored %d out of %d. \n", correct, len(problems))
-	}
-}
 
-func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: strings.TrimSpace(line[1]),
+			return score, nil
 		}
 	}
-	return ret
-}
-
-type problem struct {
-	q string
-	a string
-}
-
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
 }
